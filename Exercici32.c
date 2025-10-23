7fcb6502b0d6c9f21e97850ca744514dbbc46513
Jaumix3 <topotamadre1811@gmail.com>
2025-10-23 12:54:36 +0200

    QuickShell-config

--- Exercici32.c
+++ Exercici32.c
@@ -1,25 +1,26 @@
 //Fes un programa que demani 5 números i al final et mostri la mitja dels 5 números.
-// int main()
-// {
-    // int n1, n2, n3, n4, n5;
-    // printf("Diguis un primer nombre: \n");
-    // scanf("%d",&n1);
-    // printf("Diguis un segon nombre: \n");
-    // scanf("%d",&n2);
-    // printf("Diguis un tercer nombre: \n");
-    // scanf("%d",&n3);
-    // printf("Diguis un quart nombre: \n");
-    // scanf("%d",&n4);
-    // printf("Diguis un cinquè nombre: \n");
-    // scanf("%d",&n5);   
-// }
+
 #include <stdio.h>
 
-int main(){
+int main()
+ {
+    int n1, n2, n3, n4, n5;
+    int sumN;
+    int resultat;
+     printf("Diguis un primer nombre: \n");
+     scanf("%d",&n1);
+     printf("Diguis un segon nombre: \n");
+     scanf("%d",&n2);
+     printf("Diguis un tercer nombre: \n");
+     scanf("%d",&n3);
+     printf("Diguis un quart nombre: \n");
+     scanf("%d",&n4);
+     printf("Diguis un cinquè nombre: \n");
+     scanf("%d",&n5);   
     
-    for()
-    {
+    sumN=n1+n2+n3+n4+n5;
+    resultat=sumN/5;
+    printf("La mitja aritmètica dels 5 nombres és %d\n", resultat);
+ }
 
-    }
-}
-
+ 
---
+++ Exercici33.c
@@ -0,0 +1,22 @@
+//Fer un programa que vagi demanant nombres a l’usuari i seguidament pregunti si en vol introduir un altre. En el cas que l’usuari digui que no, mostri la seva suma.
+
+#include <stdio.h>
+
+int main()
+{
+    int a=1;
+    int n; 
+    char op;
+    int sum;
+    do  
+    {
+        printf("Digues alguns nombres: \n");
+        scanf("%d",&n);
+        sum=sum+n;
+        printf("Vols continuar? S/n \n");
+        fflush(stdin);
+        scanf(" %c",&op);                       
+    } while(op=='S' || op=='s'); 
+    printf("%d \n",sum);
+}
+
