cef12c0153faf6a01ee3ce2a5e3ea96e596e1a39
Jaumix3 <topotamadre1811@gmail.com>
2025-11-27 13:23:31 +0100

    upto47

--- C files/Exercici46.c
+++ C files/Exercici46.c
@@ -8,5 +8,44 @@ Num. De A: 2, num E: 0, num I: 0, num. O: 2, num. U: 0. Hi ha 4 vocals
 
 int main(){
     char paraula[128];
-    
+    int p;
+    int a=0, e=0, i=0, o=0, u=0;
+    printf("Digues una paraula: ");
+    fgets(paraula, sizeof(paraula), stdin);
+    strtok(paraula, "\n");
+    for(p=0; p<strlen(paraula); p++){
+        switch(paraula[p]){
+            case 'a':
+            case 'A':
+                a++;
+                break;
+            case 'e':
+            case 'E':
+                e++;
+                break;
+            case 'i':
+            case 'I':
+                i++;
+                break;
+            case 'o':
+            case 'O':
+                o++;
+                break;
+            case 'u':
+            case 'U':
+                u++;
+                break;
+            default:
+                break;
+        }
+    }
+            printf("Num. De A: %d, num E: %d, num I: %d, num. O: %d, num. U: %d. Hi ha %d vocals", a, e, i, o, u, a+e+i+o+u);
+
 }
+
+// if(paraula[i]=='a' || paraula[i]=='e' ||paraula[i]=='i' ||paraula[i]=='o' ||paraula[i]=='u'){
+    // printf("");
+// }
+// else{
+    // printf("%c", paraula[i]);
+// }
